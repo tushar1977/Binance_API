b8b9c96e77a5e4160dfbe2235e4557ed7e30656f
tushar1977 <36126341+tushar1977@users.noreply.github.com>
2023-10-02 20:19:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ from binance.client import Client
 API_KEY = '' #your api key
 SECT_KEY = '' #your secret key
 
-# API_KEY = 'V0HELY9T96IglGH4BF2cy7GHu3ynZviIhkHGMwZwjnjz5D3ksgcMghVumJqHIjNj'
-# SECT_KEY = 'FAnLa3tLddQE1MbiSTAg6kuIVimS5DXx2bRdEYNlyZLHsnRrqGhQKffj5IbxiUIj'
-
 
 client = Client(API_KEY, SECT_KEY ,tld='com')
 
